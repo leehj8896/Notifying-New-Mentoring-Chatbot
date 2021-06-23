2390bf4c1a81a3de9ec5b6f2b4d5f72384adbf2e
leehj8896 <leehj8896@gmail.com>
2021-06-23 12:38:12 +0900

    headless로 수정

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ today = datetime.datetime.now().now()
 try:
     # 접속
     options = webdriver.ChromeOptions()
-    # options.add_argument('headless')
+    options.add_argument('headless')
     options.add_argument('window-size=1920x1080')
     options.add_argument("disable-gpu")
     options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
